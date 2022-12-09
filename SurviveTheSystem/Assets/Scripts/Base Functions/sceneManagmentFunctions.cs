f6b0a174c4088e432ac531ae2e9d723fc7568062
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2022-12-09 17:28:06 -0500

    Created Basic Scenes for demo, and started bascic scripts

---
+++ SurviveTheSystem/Assets/Scripts/Base Functions/sceneManagmentFunctions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManagmentFunctions : MonoBehaviour
+{
+    public static void loadScene(int sceneIndex)
+    {
+        
+        if(sceneIndex > SceneManager.sceneCount)
+        {
+            Debug.LogWarning("There is no scene with an index of " + sceneIndex);
+            Debug.LogWarning("Please make sure all scenes are loaded in the build settings before trying again");
+            return;
+        }
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public static void quitGame()
+    {
+        if (Application.isEditor) Debug.LogWarning("Should Be Quitting, but is playing in editor");
+        Application.Quit();
+    }
+
+}
---
+++ SurviveTheSystem/Assets/Scripts/Manager Scripts/playerHealth.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour
+{
+    public int maxHearts = 3;
+    public int currentHearts;
+    public Image healthImage;
+
+}
