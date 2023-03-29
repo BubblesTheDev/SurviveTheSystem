3d23754cac164f830b9a90d7dfb81b1d99aa3800
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2023-03-28 21:14:01 -0400

    Changed experience handler

--- SurviveTheSystem/Assets/Scripts/ADHD Scenario/experienceHandler.cs
+++ SurviveTheSystem/Assets/Scripts/ADHD Scenario/experienceHandler.cs
@@ -4,29 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class experienceHandler : MonoBehaviour
-{
+public class experienceHandler : MonoBehaviour {
     public static experienceHandler current;
     public List<experienceStats> experiences;
+    [SerializeField] private float minTimeBetweenExp, maxTimeBetweenExp;
+    [SerializeField] private bool isPlaying;
+    private float currentTime;
 
-    private void Awake()
-    {
+    private void Awake() {
         current = this;
     }
 
-    public event Action<string> onExpereienceCall;
-    public void callExperience(string experienceToCall)
-    {
+    public IEnumerator callExperience(string experienceToCall) {
+
+        isPlaying = true;
+
         //finds the current experience where its name matches the experience to call, then calls its spesific action
-        //current.experiences.Where(experiences => experiences.name == experienceToCall).FirstOrDefault().experienceObject.playExperience();
+        current.experiences.Where(experiences => experiences.name == experienceToCall).FirstOrDefault().experienceObject.playExperience(experienceToCall);
+        if (current.experiences.Where(experiences => experiences.name == experienceToCall).FirstOrDefault().isLimited) experiences.Remove(current.experiences.Where(experiences => experiences.name == experienceToCall).FirstOrDefault());
+
+
+        currentTime = UnityEngine.Random.Range(minTimeBetweenExp, maxTimeBetweenExp);
+        yield return new WaitForSeconds(currentTime);
+        isPlaying = false;
     }
 }
 
 [System.Serializable]
-public struct experienceStats
-{
+public struct experienceStats {
     public string name;
     public experienceBase experienceObject;
-    public bool isPlaying;
     public float experienceChance;
+    public bool isLimited;
 }
